993424b8a7e21354b779f4b40a482be33f134341
MADhu19-509 <91412564+MADhu19-509@users.noreply.github.com>
2023-10-18 16:28:01 +0530

    Add files via upload

---
+++ descending.js
@@ -0,0 +1,5 @@
+let array=[1,5,6,3,2,4,5,67,8,9];
+array.sort(function (a,b ) {
+    return b-a
+});
+console.log(array);
---
+++ reverse.js
@@ -0,0 +1,5 @@
+function strrev(str)
+    {
+        return str.split(' ').reverse().join(' ')
+    }
+console.log(strrev("This is a sunny day"))
